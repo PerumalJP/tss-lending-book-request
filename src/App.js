fc2840f3c636e76604d6fb14fd6b8b54e65c617b
Perumal Vedhapuri <M1058123@mindtree.com>
2021-06-17 01:06:49 +0530

    App initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Login from './components/login';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      
+      {/* <header className="app-header">
+        Lending books
+      </header> */}
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/login/index.js
@@ -0,0 +1,49 @@
+
+import './index.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Login = () => {
+
+    return (
+        <Card style={{ width: '390px', height: '350px' }} className="m-auto">
+            <Card.Img style={{ height: '56px' }} variant="top" src="tss-logo.png" />
+            <Card.Header className="text-center">LENDING BOOKS REQUEST APPROVAL</Card.Header>
+            <Card.Body>
+                <Card.Title className="text-center">LOGIN</Card.Title>
+
+                <Form>
+                    <Form.Group as={Row} controlId="username">
+                        <Form.Label column sm={4} className="mandatory">
+                            Username
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="text" placeholder="Username" />
+                        </Col>
+                    </Form.Group>
+
+                    <Form.Group as={Row} controlId="password">
+                        <Form.Label column sm={4} className="mandatory">
+                            Password
+                        </Form.Label>
+                        <Col sm={8}>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Col>
+                    </Form.Group>
+                    <Col className="text-center">
+                        <Button variant="primary">Submit</Button>
+                    </Col>
+                    <Col >
+                        <Button variant="link">Forgot Password?</Button>
+                        <Button variant="link">Change Password?</Button>
+                    </Col>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Login;
