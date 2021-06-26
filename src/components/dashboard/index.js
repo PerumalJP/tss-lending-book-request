db9a9d95c36ba3c4dca63995af43174ca32385bc
Perumal Vedhapuri <M1058123@mindtree.com>
2021-06-27 01:15:38 +0530

    Adding meta tags

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,11 +1,19 @@
 import './index.css';
-
+import { MetaTags } from 'react-meta-tags';
 import { Button, Badge } from 'react-bootstrap';
 
 const Dashboard = () => {
 
     return (
         <div className="app-page">
+
+            <MetaTags id="3">
+                <title>Dashboard | Lending Books</title>
+                <meta name="description" content="Lending books for students dashboard" />
+                <meta name="keywork" content="Books, Lending books, Search books"/>
+                <meta property="og:title" content="Lending books dashboard" />
+
+            </MetaTags>
 
             <div className="status-panel">
                 <div className="status-panel-item">
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -7,6 +7,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { useContext } from 'react';
 import { AppContext } from '../../appContext';
+import MetaTags from 'react-meta-tags';
 
 const Login = () => {
 
@@ -17,40 +18,48 @@ const Login = () => {
     }
 
     return (
-        <Card style={{ width: '390px', height: 'auto' }} className="m-auto">
-            <Card.Img style={{ height: '56px' }} variant="top" src="./tss-logo.PNG" />
-            <Card.Header className="text-center">LENDING BOOKS REQUEST APPROVAL</Card.Header>
-            <Card.Body>
-                <Card.Title className="text-center">LOGIN</Card.Title>
+        <>
+            <MetaTags id="2">
+                <title>Login | Lending Books</title>
+                <meta name="description" content="Lending books for students login page" />
+                <meta name="keywork" content="Books, Lending books, Search books"/>
+                <meta property="og:title" content="Lending books login page" />
+            </MetaTags>
+            <Card style={{ width: '390px', height: 'auto' }} className="m-auto">
+                <Card.Img style={{ height: '56px' }} variant="top" src="./tss-logo.PNG" />
+                <Card.Header className="text-center">LENDING BOOKS REQUEST APPROVAL</Card.Header>
+                <Card.Body>
+                    <Card.Title className="text-center">LOGIN</Card.Title>
 
-                <Form>
-                    <Form.Group as={Row} controlId="username">
-                        <Form.Label column sm={4} className="mandatory">
-                            Username
-                        </Form.Label>
-                        <Col sm={8}>
-                            <Form.Control autoFocus={true} type="text" placeholder="Username" />
-                        </Col>
-                    </Form.Group>
+                    <Form>
+                        <Form.Group as={Row} controlId="username">
+                            <Form.Label column sm={4} className="mandatory">
+                                Username
+                            </Form.Label>
+                            <Col sm={8}>
+                                <Form.Control autoFocus={true} type="text" placeholder="Username" />
+                            </Col>
+                        </Form.Group>
 
-                    <Form.Group as={Row} controlId="password">
-                        <Form.Label column sm={4} className="mandatory">
-                            Password
-                        </Form.Label>
-                        <Col sm={8}>
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group as={Row} controlId="password">
+                            <Form.Label column sm={4} className="mandatory">
+                                Password
+                            </Form.Label>
+                            <Col sm={8}>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Col>
+                        </Form.Group>
+                        <Col className="text-center">
+                            <Button variant="primary" onClick={handleLogin}>Submit</Button>
                         </Col>
-                    </Form.Group>
-                    <Col className="text-center">
-                        <Button variant="primary" onClick={handleLogin}>Submit</Button>
-                    </Col>
-                    <Col className="mt-4">
-                        <Button variant="link">Forgot Password?</Button>
-                        <Button variant="link">Change Password?</Button>
-                    </Col>
-                </Form>
-            </Card.Body>
-        </Card>
+                        <Col className="mt-4">
+                            <Button variant="link">Forgot Password?</Button>
+                            <Button variant="link">Change Password?</Button>
+                        </Col>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 
